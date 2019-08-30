be7460793790b7c899802ec26c7f30a305cbc511
Dave Vazquez <dmvaz.li@gmail.com>
2019-08-29 22:39:54 -0400

    added test for emoji.combine()

--- __tests__/emoji_tests.js
+++ __tests__/emoji_tests.js
@@ -4,10 +4,16 @@ describe('emoji functions', () => {
       expect(emoji.get('smile')).toBe('😄');
     });
     it('should return a sad emoji', () => {
-      expect(emoji.get('smile')).toBe('😢');
+      expect(emoji.get('sad')).toBe('😢');
     });
   });
   it('should return a random emoji', () => {
     expect(emoji.get()).anything();
+  });
+
+  describe('combine emojis', () => {
+    it('should return an expressionless emoji', () => {
+      expect(emoji.combine('smile', 'sad')).toBe('😑');
+    });
   });
 });
